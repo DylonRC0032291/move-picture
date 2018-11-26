8e0959c52dc9d1d83631a64e85c82c2c80baceda
Dylon Forshaw <rc0032291@student.riversidecollege.ac.uk>
2018-11-26 15:42:19 +0000

    poo

--- move picture/Form1.Designer.cs
+++ move picture/Form1.Designer.cs
@@ -34,9 +34,9 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button7 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
@@ -45,9 +45,9 @@
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(173, 50);
+            this.button1.Location = new System.Drawing.Point(173, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 41);
+            this.button1.Size = new System.Drawing.Size(80, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "move right";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,7 +56,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::move_picture.Properties.Resources.The_Simpsons_Jeff_Albertson;
-            this.pictureBox1.Location = new System.Drawing.Point(336, 45);
+            this.pictureBox1.Location = new System.Drawing.Point(343, 109);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(156, 212);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -66,9 +66,9 @@
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(3, 50);
+            this.button2.Location = new System.Drawing.Point(3, 51);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(79, 41);
+            this.button2.Size = new System.Drawing.Size(79, 42);
             this.button2.TabIndex = 2;
             this.button2.Text = "move left";
             this.button2.UseVisualStyleBackColor = true;
@@ -79,7 +79,7 @@
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Location = new System.Drawing.Point(88, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(79, 41);
+            this.button3.Size = new System.Drawing.Size(79, 42);
             this.button3.TabIndex = 3;
             this.button3.Text = "move up";
             this.button3.UseVisualStyleBackColor = true;
@@ -88,9 +88,9 @@
             // button4
             // 
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(88, 97);
+            this.button4.Location = new System.Drawing.Point(88, 99);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(79, 44);
+            this.button4.Size = new System.Drawing.Size(79, 42);
             this.button4.TabIndex = 4;
             this.button4.Text = "move down";
             this.button4.UseVisualStyleBackColor = true;
@@ -102,7 +102,9 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.button8, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 2);
             this.tableLayoutPanel1.Controls.Add(this.button7, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.button4, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.button1, 2, 1);
@@ -117,12 +119,23 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(256, 144);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(173, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 42);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "north east";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // button5
             // 
             this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button5.Location = new System.Drawing.Point(3, 3);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(79, 41);
+            this.button5.Size = new System.Drawing.Size(79, 42);
             this.button5.TabIndex = 0;
             this.button5.Text = "north west";
             this.button5.UseVisualStyleBackColor = true;
@@ -130,45 +143,36 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(446, 311);
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(173, 99);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(80, 42);
             this.button6.TabIndex = 0;
-            this.button6.Text = "south west";
+            this.button6.Text = "South east";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // button7
-            // 
-            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button7.Location = new System.Drawing.Point(173, 3);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(80, 41);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "north east";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(518, 413);
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Location = new System.Drawing.Point(3, 99);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.Size = new System.Drawing.Size(79, 42);
             this.button8.TabIndex = 7;
-            this.button8.Text = "south east";
+            this.button8.Text = "south west";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button6);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = ".";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -19,38 +19,55 @@ namespace move_picture
 
         private void button1_Click(object sender, EventArgs e)
         {
-          pictureBox1.Location = new Point (pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            pictureBox1.Location = new Point (pictureBox1.Location.X + 10, pictureBox1.Location.Y);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Location.X > 0)
             pictureBox1.Location = new Point (pictureBox1.Location.X - 10, pictureBox1.Location.Y);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Location.Y > 0)
             pictureBox1.Location = new Point (pictureBox1.Location.X, pictureBox1.Location.Y - 10 );
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Location.Y - 5 < ClientRectangle.Height - pictureBox1.Height)
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 10);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Location.Y > 0) if (pictureBox1.Location.X > 0 )
             pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y - 5);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y - 5 );
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 10 );
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
+            if (pictureBox1.Location.Y > 0 ) if (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y - 5);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 10);
         }
     }
 }
